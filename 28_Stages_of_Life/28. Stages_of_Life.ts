4e7b812738212d885a5a6da3e0c87339abe7fa39
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-26 00:23:21 +0500

    Add files via upload

---
+++ 28_Stages_of_Life/28. Stages_of_Life.ts
@@ -0,0 +1,39 @@
+// Write an if-else chain that determines a person’s stage of life.
+// Set a value for the variable age.
+let age: number = 18;
+
+// If the person is less than 2 years old.
+// Print a message that the person is a baby.
+if(age < 2){
+    console.log("person is a baby.")
+}
+
+// If the person is at least 2 years old but less than 4.
+// Print a message that the person is a toddler.
+else if(age >= 2 && age < 4){
+    console.log("person is a toddler.")
+}
+
+// If the person is at least 4 years old but less than 13.
+// Print a message that the person is a kid.
+else if(age >=4 && age < 13){
+    console.log("person is a kid.")
+}
+
+// If the person is at least 13 years old but less than 20.
+// Print a message that the person is a teenager.
+else if(age >=13 && age < 20){
+    console.log("person is a teenager.")
+}
+
+// If the person is at least 20 years old but less than 65.
+// Print a message that the person is an adult.
+else if(age >=20 && age < 65){
+    console.log("person is an adult.")
+}
+
+// If the person is age 65 or older.
+// Print a message that the person is an elder.
+else{
+    console.log("person is an elder")
+}
