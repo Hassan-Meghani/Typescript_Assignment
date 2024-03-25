699a4af7be4900059d9654faa4355ff02899483a
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-26 00:06:44 +0500

    Add files via upload

---
+++ 13_Your_Own_Array/13. Your_Own_Array.js
@@ -0,0 +1,5 @@
+"use strict";
+let transportation = ['Island', 'Car', 'Yacht', 'Private Jet'];
+for (let i = 0; i < transportation.length; i++) {
+    console.log('I would like to own a ' + transportation[i]);
+}
