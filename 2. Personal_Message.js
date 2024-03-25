04f309431225aeeccd33aadbb3e7a40b7b4dabf5
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-25 23:49:41 +0500

    Delete 2. Personal_Message.js

--- 2. Personal_Message.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let personName = '';
-personName = prompt('What is your name?') || '';
-if (personName !== null && personName !== '') {
-    alert(` Hello ${personName}, how are you today?`);
-}
-else {
-    alert(`You have to fill your name !`);
-}
