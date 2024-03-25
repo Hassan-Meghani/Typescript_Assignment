11c061c236e83bf8678b9d89bf82b0e50d3e6a4c
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-26 00:04:25 +0500

    Add files via upload

---
+++ 11_Names/11. Names.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Abdullah', 'Umair', 'Bisma', 'Taha', 'Jasbeer'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11_Names/11. Names.ts
@@ -0,0 +1,4 @@
+let names :string[] = ['Abdullah','Umair','Bisma','Taha','Jasbeer'] ;
+for(let i=0; i<names.length; i++){
+    console.log(names[i]) ;
+}
