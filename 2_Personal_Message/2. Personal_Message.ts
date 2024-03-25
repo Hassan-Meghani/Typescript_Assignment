da16854fc3d62107182d66920c647a4a25c70f99
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-25 23:48:23 +0500

    Add files via upload

---
+++ 2_Personal_Message/2. Personal_Message.ts
@@ -0,0 +1,7 @@
+let personName :string = '';
+personName = prompt('What is your name?') || '';
+if(personName !== null && personName !== ''){
+    alert(` Hello ${personName}, how are you today?`)
+}else{
+    alert(`You have to fill your name !`)
+}
