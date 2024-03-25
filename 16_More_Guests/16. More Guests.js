5b48dc2a6c7513be2035bb958f55eb500cf73923
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-26 00:10:43 +0500

    Add files via upload

---
+++ 16_More_Guests/16. More Guests.js
@@ -0,0 +1,19 @@
+"use strict";
+let Guest_list = ['Ahmed Shahid', 'Taha Khan', 'Hussain Ali'];
+//for(let i=0; i<Guest_list.length; i++){
+//console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is my pleasure to invite you in a small get together.\n\nThank You!\n\n')
+//}
+let absent_Guest = 'Ahmed Shahid';
+let new_Guest = 'Munir Ahmed';
+Guest_list[0] = new_Guest;
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is my pleasure to invite you in a small get together.\n\nThank You!\n\n');
+}
+console.log(`Mr. ${absent_Guest} is not coming to the get together.`);
+console.log('Good News! We found a Big Space So we are inviting 3 more guests.');
+Guest_list.unshift('Mr Rafay Jalbani');
+Guest_list.splice(2, 0, 'Bisma Sultana');
+Guest_list.push('Maryam Irshad');
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is my pleasure to invite you in a small get together.\n\nThank You!\n\n');
+}
---
+++ 16_More_Guests/16. More Guests.ts
@@ -0,0 +1,18 @@
+let Guest_list :string[] = ['Ahmed Shahid','Taha Khan','Hussain Ali'];
+//for(let i=0; i<Guest_list.length; i++){
+    //console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is my pleasure to invite you in a small get together.\n\nThank You!\n\n')
+//}
+let absent_Guest :string = 'Ahmed Shahid';
+let new_Guest :string = 'Munir Ahmed';
+Guest_list[0] = new_Guest ;
+for(let i=0; i<Guest_list.length; i++){
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is my pleasure to invite you in a small get together.\n\nThank You!\n\n')
+}
+console.log(`Mr. ${absent_Guest} is not coming to the get together.`);
+console.log('Good News! We found a Big Space So we are inviting 3 more guests.')
+Guest_list.unshift('Mr Rafay Jalbani')
+Guest_list.splice(2 , 0 , 'Bisma Sultana');
+Guest_list. push('Maryam Irshad');
+for(let i=0; i<Guest_list.length; i++){
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is my pleasure to invite you in a small get together.\n\nThank You!\n\n')
+}
