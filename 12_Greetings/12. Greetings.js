0e29c830a59cd89c5d510e3ad3ca197a31b5d8f3
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-26 00:05:50 +0500

    Add files via upload

---
+++ 12_Greetings/12. Greetings.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Abdullah', 'Umair', 'Bisma', 'Taha', 'Jasbeer'];
+const message = 'Hello, Good to see you: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12_Greetings/12. Greetings.ts
@@ -0,0 +1,5 @@
+let names :string[] = ['Abdullah','Umair','Bisma','Taha','Jasbeer'] ;
+const message :string = 'Hello, Good to see you: '
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]) ;
+}
