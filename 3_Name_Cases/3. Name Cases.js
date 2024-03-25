bf489a668cb5f6364c628a2b80f556113f0b900a
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-25 23:52:19 +0500

    Add files via upload

---
+++ 3_Name_Cases/3. Name Cases.js
@@ -0,0 +1,15 @@
+"use strict";
+let personeName = '';
+personeName = prompt('What is your name?') || '';
+let lowercase = personeName.toLowerCase();
+let uppercase = personeName.toUpperCase();
+let titlecase = personeName.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
+if (personeName !== null && personeName !== '') {
+    alert(`Hello ${personeName},Here are your name in:
+LowerCase: ${lowercase}
+UpperCase: ${uppercase}
+TitleCase: ${titlecase}`);
+}
+else {
+    alert('Please fill your name !');
+}
