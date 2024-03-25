9a2b3db8a4f9336171da781bf526421cd6b0bf0b
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-26 00:15:33 +0500

    Add files via upload

---
+++ 20_Empty_Array/20. Empty_Array.js
@@ -0,0 +1,9 @@
+"use strict";
+// Think of something you could store in a array.
+let items = ['Belgium', 'Brussel', 'French'];
+// Write a program that creates a list containing these items.
+let item2 = [];
+item2.push('Rome');
+item2.push('IceLand');
+item2.push('Australia');
+console.log(item2);
---
+++ 20_Empty_Array/20. Empty_Array.ts
@@ -0,0 +1,10 @@
+// Think of something you could store in a array.
+let items :string[] = ['Belgium', 'Brussel','French'];
+
+// Write a program that creates a list containing these items.
+let item2 :string[] = [];
+item2.push('Rome');
+item2.push('IceLand');
+item2.push('Australia');
+
+ console.log(item2);
