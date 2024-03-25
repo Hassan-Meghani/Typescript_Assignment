e8906f5ff58a30cf6dd097733a2e02402b1c75fb
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-26 00:17:43 +0500

    Add files via upload

---
+++ 22_Intentional_Error/22. Intentional_Error.js
@@ -0,0 +1,9 @@
+"use strict";
+// If you haven’t received an array index error in one of your programs yet, try to make one happen.
+// Creating array.
+let newArray = ['Jaun Ellia', 'Tom Cruise', 'Andrew Tate'];
+// Accessing Array by index.
+console.log(newArray[5]);
+// Make sure you correct the error before closing the program.
+// Correct the errpr.
+console.log(newArray);
