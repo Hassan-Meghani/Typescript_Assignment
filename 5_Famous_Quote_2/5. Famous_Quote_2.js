f4416b3d87afc9e1321b475fc7ff2b2b9f666b54
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-25 23:57:08 +0500

    Add files via upload

---
+++ 5_Famous_Quote_2/5. Famous_Quote_2.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = 'Albert Einstein';
+let message = '"In the middle of difficulty lies opportunity."';
+console.log(`${famous_person} once said, ${message}`);
---
+++ 5_Famous_Quote_2/5. Famous_Quote_2.ts
@@ -0,0 +1,3 @@
+let famous_person :string = 'Albert Einstein';
+let message :string = '"In the middle of difficulty lies opportunity."';
+console.log(`${famous_person} once said, ${message}`);
