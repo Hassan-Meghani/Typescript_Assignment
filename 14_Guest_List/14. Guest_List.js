aa29114df23299c90eb04934bd04584f59c8a4a4
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-26 00:08:02 +0500

    Add files via upload

---
+++ 14_Guest_List/14. Guest_List.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_list = ['Ahmed Shahid', 'Taha Khan', 'Hussain Ali'];
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is my pleasure to invite you in a small get together.\n\nThank You!');
+}
---
+++ 14_Guest_List/14. Guest_List.ts
@@ -0,0 +1,4 @@
+let Guest_list :string[] = ['Ahmed Shahid','Taha Khan','Hussain Ali'];
+for(let i=0; i<Guest_list.length; i++){
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is my pleasure to invite you in a small get together.\n\nThank You!')
+}
