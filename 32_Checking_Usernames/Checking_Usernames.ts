6e728e700d0bde176addfed0d80194bb6d9616a7
Hassan-Meghani <164177470+Hassan-Meghani@users.noreply.github.com>
2024-03-26 23:05:55 +0500

    Add files via upload

---
+++ 32_Checking_Usernames/Checking_Usernames.ts
@@ -0,0 +1,24 @@
+// Make a list of five or more usernames called current_users.
+let current_users: string[] = ['HassaN','HAdi','TaHa','Ahmed','John'];
+
+// Make another list of five usernames called new_users.
+// // Make sure one or two of the new usernames are also in the current_users list.
+let new_users: string[] = ['Ibrahim','HassaN','Shabbir','Hamza','Ahmed'];
+
+// Loop through the new_users list to see if each new username has already been used.
+// If it has, print a message that the person will need to enter a new username.
+// If a username has not been used.
+// Print a message saying that the username is available.
+new_users.forEach(newUsername => {
+    let lowerCase: string = newUsername.toLowerCase();
+
+    if(current_users.map(c_user => c_user.toLowerCase()).includes(lowerCase)){
+        console.log(`The username ${newUsername} is already in use. Please try different`);
+    }
+    else{
+        console.log(`The username ${newUsername} is available.`);
+    }
+})
+
+// Make sure your comparison is case insensitive.
+// If 'John' has been used, 'JOHN' should not be accepted.
