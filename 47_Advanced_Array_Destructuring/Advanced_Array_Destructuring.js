bf1575dce0c62fa8b500913f2de62a9a7d2866d7
Hassan Meghani <164177470+hassanmeghani@users.noreply.github.com>
2024-06-06 19:21:05 +0500

    Add files via upload

---
+++ 47_Advanced_Array_Destructuring/Advanced_Array_Destructuring.js
@@ -0,0 +1,16 @@
+"use strict";
+// Given an array of objects representing different laptops.
+// Each with properties make, model, and year.
+// Use array destructuring to assign the first and second laptops to variables.
+// Then, log these variables.
+// Explain & TIP: 
+// Destructuring can also be used to quickly extract elements from arrays of objects.
+// Simplifying access to complex data structures.
+let laptops = [
+    { make: "Acer", model: "XPS 15", year: 2020 },
+    { make: "Apple", model: "MacBook Pro", year: 2023 },
+    { make: "HP", model: "Spectre x360", year: 2022 }
+];
+let [laptop1, laptop2] = laptops;
+console.log(laptop1);
+console.log(laptop2);
