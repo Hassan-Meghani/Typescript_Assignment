d76a4aa333f83caab27f9f0808993a5c2f9b81f4
Hassan Meghani <164177470+hassanmeghani@users.noreply.github.com>
2024-06-06 19:48:07 +0500

    Add files via upload

---
+++ 50_Multiline_Template_Literals/Multiline_Template_Literals.js
@@ -0,0 +1,13 @@
+"use strict";
+//  Use template literals to create a multiline string that describes your ideal day. 
+// Include at least three different activities.
+// Explain & TIP: 
+// Template literals support multiline strings.
+// Making it easier to create readable text blocks that incorporate variables and expressions.
+// Using template literals to define a multiline string
+let myIdealDay = `My ideal day would involve:
+1. Waking up early and going for a jog.
+2. Spending a few hours coding on a personal project.
+3. Ending the day by reading a good book.`;
+// Logging the multiline string to the console
+console.log(myIdealDay);
